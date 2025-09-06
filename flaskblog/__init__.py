b39685e24a07047830a9bb1a53c6887612a78179
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2025-09-06 03:06:56 +0100

    Update

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '97692fbe06cb2d64964e42d05cc5f278'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)  # noqa
-
+bcrypt = Bcrypt(app)  # noqa
 from flaskblog import route  # noqa
